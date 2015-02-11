00904108c48ee60dde16ceda5b772f27401ccd66
slanders <slanders@ebsco.com>
2015-02-11 11:07:53 -0600

    Initial commit

---
+++ tasks/text-replace.js
@@ -0,0 +1,33 @@
+/*
+ * grunt-match-replace
+ * http://as-gitmaster.corp.epnet.com/projects/RD/repos/grunt-match-replace
+ *
+ * Copyright (c) 2015 Jonathan Holmes, Scott Landers
+ * Licensed under the MIT license.
+ */
+
+var gruntTextReplace = require('../lib/grunt-match-replace');
+
+module.exports = function(grunt) {
+
+
+
+  // Please see the grunt documentation for more information regarding task
+  // creation: https://github.com/gruntjs/grunt/blob/devel/docs/toc.md
+
+  grunt.registerMultiTask('translations',
+    'General purpose text replacement for grunt. Allows you to replace ' +
+    'text in files using strings, regexs or functions. Searches for text and' +
+    'generates console output and optional text report of matches found',
+    function () {
+      gruntTextReplace.match({
+        src: this.data.src,
+        dest: this.data.dest,
+        overwrite: this.data.overwrite,
+        replacements: this.data.replacements,
+        reportPath: this.data.reportPath,
+        translationAttributes: this.data.translationAttributes,
+        exclusionFile: this.data.exclusionFile
+      });
+    });
+};
